85a6386c0f78b7ab61f98ba111e8b53418fbe7e1
Viktoria Melnyk <melnukvikysa@gmail.com>
2026-09-15 19:46:16 +0300

    Implemented the validation function

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,13 +12,42 @@ function pow(base, exponent) {
 }
 try {
   console.log(pow(3, 3));
-  console.log(pow("jdbu", 3))
+  console.log(pow("jdbu", 3));
 } catch (err) {
   console.log(err);
 }
-try{
+try {
   console.log(pow(1, 1001));
-}catch(err){
+} catch (err) {
   console.log(err);
 }
-console.log(pow(1,1));
+try {
+  console.log(pow(1, 1));
+} catch (err) {
+  console.log(err);
+}
+
+function emailValidation(email) {
+  if (typeof email !== "string" || !email.includes("@")) {
+    throw new TypeError("Email must be a string and contain @.");
+  }
+  if (email.startsWith("@") || email.endsWith("@")) {
+    throw new RangeError("@ must not be the first or the last.");
+  }
+  return email;
+}
+try {
+  console.log(emailValidation("hbuefgr"));
+} catch (err) {
+  console.log(err);
+}
+try {
+  console.log(emailValidation("hbuefgr@"));
+} catch (err) {
+  console.log(err);
+}
+try {
+  console.log(emailValidation("hbuefgr@bcdj"));
+} catch (err) {
+  console.log(err);
+}
